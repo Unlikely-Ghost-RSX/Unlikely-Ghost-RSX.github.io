24089966304fa07724fa596f6b20d9afc80f348f
Unlikely-Ghost-RSX <stirgus63192@students.apsb.org>
2023-03-24 13:54:36 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,12 +21,20 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
-
+        var circle
+        var circles=[]
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            circle=draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        for(var i=0;i<100;i++){
+            drawCircle()
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,11 +47,13 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            for(i=0;i<100;i++){
+            physikz.updatePosition(circles[i])
+            game.checkCirclePosition(circles[i])
+            }
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            
             // TODO 9 : Iterate over the array
            
             
@@ -62,7 +72,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if(circle.x<0){
+                circle.x=canvas.width;
+            }
+            if(circle.y>canvas.height){
+                circle.y=0;
+            }
+            if(circle.y<0){
+                circle.y=canvas.height;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
